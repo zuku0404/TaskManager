9a28eb2c4e317d31a7a3843e2edb5dc52a6e75a6
mateusz <mzuk0493@gmail.com>
2024-04-08 15:17:16 +0200

    clear gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,9 +5,13 @@ plugins {
 
 group = "com.zuku"
 version = "1.0-SNAPSHOT"
+
 val mySqlVersion = "8.0.28"
 val koinVersion = "3.1.4"
 val kotestVesrion = "5.8.0"
+val gsonVersion = "2.10"
+val mockkVersion = "1.12.4"
+val jUnitVersion = "5.10.1"
 
 repositories {
     mavenCentral()
@@ -16,10 +20,10 @@ repositories {
 dependencies {
     implementation("mysql:mysql-connector-java:$mySqlVersion")
     implementation("io.insert-koin:koin-core:$koinVersion")
+    implementation("com.google.code.gson:gson:$gsonVersion")
     testImplementation ("io.kotest:kotest-runner-junit5-jvm:$kotestVesrion")
-    implementation("com.google.code.gson:gson:2.10")
-    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.10.1")
-    testImplementation("io.mockk:mockk:1.12.4")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:$jUnitVersion")
+    testImplementation("io.mockk:mockk:$mockkVersion")
     testImplementation(kotlin("test"))
 }
 
